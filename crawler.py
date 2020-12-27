8250761a7df84397cec17c9b344e0c9656b1de5b
Huskyee <50997488+Huskyee@users.noreply.github.com>
2020-12-27 21:33:41 +0800

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,92 @@
+import json
+import re
+import requests
+
+
+# 擷取「上課時間及教室」之「時間」部分
+def parse_time(tc):
+    pattern = '[MTWRFSU][1-9yznabcd]+'
+    tc_list = tc.split(',')
+    time_list = []
+    for item in tc_list:
+        time = re.findall(pattern, item.split('-')[0])
+        for t in time:
+            time_list.append(t)
+    return time_list
+
+
+year = 109
+semester = 2
+
+url = "https://timetable.nctu.edu.tw/?r=main/get_cos_list"
+# User-Agent 請求標頭（request header）含有能令網路協議同級層（peer）識別發出該用戶代理請求的軟體類型或版本號、該軟體使用的作業系統、還有軟體開發者的字詞串。
+headers = {"user-agent": "Mozilla/5.0"}
+data = {"m_acy": year,          # 學年度(開始)
+        "m_sem": semester,      # 學期別(開始)
+        "m_acyend": year,       # 學年度(結束)
+        "m_semend": semester,   # 學期別(結束)
+        "m_dep_uid": "**",
+        "m_group": "**",
+        "m_grade": "**",
+        "m_class": "**",
+        "m_option": "**",
+        "m_crsname": "**",
+        "m_teaname": "**",
+        "m_cos_id": "**",
+        "m_cos_code": "**",
+        "m_crstime": "**",
+        "m_crsoutline": "**",
+        "m_costype": "**"}
+
+# 資料型態為 json
+r = requests.post(url, headers=headers, data=data)
+
+# requests.codes.ok = 200
+if r.status_code != requests.codes.ok:
+    print("Data request error!")
+    exit()
+
+# 將 json 轉換為 python 可處理之資料型態
+raw_data = json.loads(r.text)
+course_data = {}
+for dep_value in raw_data:
+    language = raw_data[dep_value]["language"]
+    for dep_content in raw_data[dep_value]:
+        # if dep_content == "costype":
+        #   pass
+
+        # 課程資料在 dep_content 為 1 或 2 之物件裡
+        if re.match("^[1-2]+$", dep_content) is None:
+            continue
+        for cos_id in raw_data[dep_value][dep_content]:
+            # 避免加入重複課程
+            if cos_id in course_data:
+                continue
+            raw_cos_data = raw_data[dep_value][dep_content][cos_id]
+            time_list = parse_time(raw_cos_data["cos_time"])
+            brief_code = list(raw_data[dep_value]["brief"][cos_id].keys())[0]
+            brief = raw_data[dep_value]["brief"][cos_id][brief_code]['brief'].split(',')
+            course_data[cos_id] = {
+                "id": raw_cos_data["cos_id"],                           # 當期課號
+                "num_limit": raw_cos_data["num_limit"],                 # 人數上限
+                "reg_num": raw_cos_data["reg_num"],                     # 修課人數
+                "name": raw_cos_data["cos_cname"],                      # 課程名稱
+                "credit": raw_cos_data["cos_credit"],                   # 學分
+                "hours": raw_cos_data["cos_hours"],                     # 時數
+                "teacher": raw_cos_data["teacher"],                     # 開課教師
+                "time": time_list,                                      # 上課時間
+                "time-classroom": raw_cos_data["cos_time"],             # 上課時間及教室
+                "english": language[cos_id]["授課語言代碼"] == "en-us",   # 英語授課
+                "brief": brief,                                         # 摘要
+                "memo": raw_cos_data["memo"],                           # 說明
+                "type": raw_cos_data["cos_type"],                       # 選別
+            }
+
+# for cos_id in course_data:
+#     print(course_data[cos_id]["id"])
+#     print(course_data[cos_id]["name"])
+#     print(course_data[cos_id]["brief"])
+#     print('\n')
+
+with open(str(year) + '-' + str(semester) + "_data.json", "w") as f:
+    f.write(json.dumps(course_data))
---
+++ final.js
@@ -0,0 +1,172 @@
+"use strict";
+
+const year = 109
+const semester = 2
+var course_data = {}
+
+function generateTable()
+{
+    const timeList = ["y", "z", "1", "2", "3", "4", "n", "5", "6", "7", "8", "9", "a", "b", "c", "d"] ;
+    const dayList = ["M", "T", "W", "R", "F", "S", "U"] ;
+    const timePeriod = ["6:00 ~ 6:50", "7:00 ~ 7:50", "8:00 ~ 8:50", "9:00 ~ 9:50", "10:10 ~ 11:00", "11:10 ~ 12:00",
+        "12:20 ~ 13:10", "13:20 ~ 14:10", "14:20 ~ 15:10", "15:30 ~ 16:20", "16:30 ~ 17:20", "17:30 ~ 18:20",
+        "18:30 ~ 19:20", "19:30 ~ 20:20", "20:30 ~ 21:20", "21:30 ~ 22:20"] ;
+    for(let i=0; i<timeList.length; i++)
+    {
+        var time = timeList[i] ;
+        var period = timePeriod[i] ;
+        var row = `<tr id="${time}"><td scope="row">${time}<br>(${period})</td>` ;
+        for(let j=0; j<dayList.length; j++)
+        {
+            var day = dayList[j]
+            row += `<td id="${day}${time}"></td>`
+        }
+        row += `</tr>`
+        $("#timetable tbody").append(row) ;
+    }
+}
+
+function autocomplete(inp, course_data) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;        
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("div");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        var result_container = document.getElementById("search_result") ;
+        result_container.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < Object.keys(course_data).length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            var key = Object.keys(course_data)[i] ;
+            var name = course_data[key]['name'] ;
+            var start_pos = name.toUpperCase().search(val.toUpperCase()) ;
+            if (start_pos != -1) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("div");
+                b.setAttribute ("class", "list-group-item list-group-item-action");
+                /*make the matching letters bold:*/
+                b.innerHTML = name ;
+                // for(let j=0; j<name.length; j++)
+                // {
+                //     if(j == start_pos)
+                //     {
+                //         var bold_substr = "<strong>" + name.substr(j, val.length) + "</strong>";
+                //         b.innerHTML += bold_substr ;
+                //         j += val.length-1 ;
+                //     }
+                //     else
+                //     {
+                //         b.innerHTML += name.substr(j, 1) ;
+                //     }
+                // }
+                // b.innerHTML = "<strong>" + arr[i].substr(start_pos, val.length) + "</strong>";
+                // b.innerHTML += arr[i].substr(val.length);
+                // /*insert a input field that will hold the current array item's value:*/
+                // b.innerHTML += "<input type='hidden' value='" + name + "'>";
+                // /*execute a function when someone clicks on the item value (DIV element):*/
+                // b.addEventListener("click", function(e) {
+                //     /*insert the value for the autocomplete text field:*/
+                //     inp.value = this.getElementsByTagName("input")[0].value;
+                //     /*close the list of autocompleted values,
+                //     (or any other open lists of autocompleted values:*/
+                //     closeAllLists();
+                // });
+                a.appendChild(b);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    // inp.addEventListener("keydown", function(e) {
+    //     var x = document.getElementById(this.id + "autocomplete-list");
+    //     if (x) x = x.getElementsByTagName("div");
+    //     if (e.keyCode == 40) {
+    //         /*If the arrow DOWN key is pressed,
+    //         increase the currentFocus variable:*/
+    //         currentFocus++;
+    //         console.log(currentFocus);
+    //         /*and and make the current item more visible:*/
+    //         addActive(x);
+    //     } else if (e.keyCode == 38) { //up
+    //         /*If the arrow UP key is pressed,
+    //         decrease the currentFocus variable:*/
+    //         currentFocus--;
+    //         console.log(currentFocus);
+    //         /*and and make the current item more visible:*/
+    //         addActive(x);
+    //     } else if (e.keyCode == 13) {
+    //         /*If the ENTER key is pressed, prevent the form from being submitted,*/
+    //         e.preventDefault();
+    //         if (currentFocus > -1) {
+    //         /*and simulate a click on the "active" item:*/
+    //         if (x) x[currentFocus].click();
+    //         }
+    //     }
+    // });
+    // function addActive(x) {
+    //     /*a function to classify an item as "active":*/
+    //     if (!x) return false;
+    //     /*start by removing the "active" class on all items:*/
+    //     removeActive(x);
+    //     if (currentFocus >= x.length) currentFocus = 0;
+    //     if (currentFocus < 0) currentFocus = (x.length - 1);
+    //     /*add class "autocomplete-active":*/
+    //     x[currentFocus].classList.add("autocomplete-active");
+    //     x[currentFocus].classList.add("bg-primary");
+    //     x[currentFocus].classList.add("text-light");
+    // }
+    // function removeActive(x) {
+    //     /*a function to remove the "active" class from all autocomplete items:*/
+    //     for (var i = 0; i < x.length; i++) {
+    //     x[i].classList.remove("autocomplete-active");
+    //     x[i].classList.remove("bg-primary");
+    //     x[i].classList.remove("text-light");
+    //     }
+    // }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                // result_container.removeChild(x[i]);
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    // document.addEventListener("click", function (e) {
+    //     closeAllLists(e.target);
+    // });
+    }
+  
+/*An array containing all the country names in the world:*/
+var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+function loadJSON()
+{
+    jQuery.ajaxSetup({async: false});
+    $.get(`${year}-${semester}_data.json`,function(data,status){
+        if(status != "success"){
+            alert("Couldn't get course data!!");
+        }
+        course_data = data;
+        // console.log(course_data);
+    });
+    jQuery.ajaxSetup({async: true});
+}
+
+$(document).ready(function(){
+    generateTable() ;/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+    loadJSON() ;
+    autocomplete(document.getElementById("search"), course_data) ;
+}) ;
