5c9ee698221c8f96e0f3a3dfa547c7cd461d6592
Chao-Kuo Chiu <huskyee.iem06@nctu.edu.tw>
2021-03-12 11:08:34 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,6 +2,14 @@ import json
 import re
 import requests
 
+year = 109
+semester = 2
+acysem = str(year) + str(semester)
+flang = "zh-tw"
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
+dep_list = []
+course_data = {}
+
 
 # 擷取「上課時間及教室」之「時間」部分
 def parse_time(tc):
@@ -30,76 +38,148 @@ def parse_classroom(tc):
     return classroom_list
 
 
-year = 109
-semester = 2
+def get_type():
+    res = requests.get('https://timetable.nycu.edu.tw/?r=main/get_type', headers=headers)
+    return res.json()
 
-url = "https://timetable.nycu.edu.tw/?r=main/get_cos_list"
-# User-Agent 請求標頭（request header）含有能令網路協議同級層（peer）識別發出該用戶代理請求的軟體類型或版本號、該軟體使用的作業系統、還有軟體開發者的字詞串。
-headers = {"user-agent": "Mozilla/5.0"}
-data = {"m_acy": year,          # 學年度(開始)
-        "m_sem": semester,      # 學期別(開始)
-        "m_acyend": year,       # 學年度(結束)
-        "m_semend": semester,   # 學期別(結束)
-        "m_dep_uid": "**",
-        "m_group": "**",
-        "m_grade": "**",
-        "m_class": "**",
-        "m_option": "**",
-        "m_crsname": "**",
-        "m_teaname": "**",
-        "m_cos_id": "**",
-        "m_cos_code": "**",
-        "m_crstime": "**",
-        "m_crsoutline": "**",
-        "m_costype": "**"}
 
-# 資料型態為 json
-r = requests.post(url, headers=headers, data=data)
+def get_category(ftype, flang, acysem):
+    res = requests.post('https://timetable.nycu.edu.tw/?r=main/get_category', data={
+        'ftype': ftype,
+        'flang': flang,
+        'acysem': acysem,
+        'acysemend': acysem
+    }, headers=headers)
+    return res.json()
 
-# requests.codes.ok = 200
-if r.status_code != requests.codes.ok:
-    print("Data request error!")
-    exit()
 
-# 將 json 轉換為 python 可處理之資料型態
-raw_data = json.loads(r.text)
-course_data = {}
-for dep_value in raw_data:
-    language = raw_data[dep_value]["language"]
-    for dep_content in raw_data[dep_value]:
-        # if dep_content == "costype":
-        #   pass
+def get_college(fcategory, ftype, flang, acysem):
+    res = requests.post('https://timetable.nycu.edu.tw/?r=main/get_college', data={
+        'fcategory': fcategory,
+        'ftype': ftype,
+        'flang': flang,
+        'acysem': acysem,
+        'acysemend': acysem
+    }, headers=headers)
+    return res.json()
 
-        # 課程資料在 dep_content 為 1 或 2 之物件裡
-        if re.match("^[1-2]+$", dep_content) is None:
-            continue
-        for cos_id in raw_data[dep_value][dep_content]:
-            # 避免加入重複課程
-            if cos_id in course_data:
+
+def get_dep(fcollege, fcategory, ftype, flang, acysem):
+    res = requests.post('https://timetable.nycu.edu.tw/?r=main/get_dep', data={
+        'fcollege': fcollege,
+        'fcategory': fcategory,
+        'ftype': ftype,
+        'flang': flang,
+        'acysem': acysem,
+        'acysemend': acysem
+    }, headers=headers)
+    return res.json()
+
+
+types = get_type()
+for i in range(len(types)):
+    ftype = types[i]["uid"]
+    # print("type: " + types[i]["cname"])
+    categories = get_category(ftype, flang, acysem)
+    for j in categories.keys():
+        if j is not "":
+            fcategory = j
+            # print("category: " + categories[fcategory])
+        else:
+            fcategory = ""
+        colleges = get_college(fcategory, ftype, flang, acysem)
+        if len(colleges):
+            for k in colleges.keys():
+                if k is not "":
+                    fcollege = k
+                    # print("college: " + colleges[fcollege])
+                else:
+                    fcollege = ""
+                deps = get_dep(fcollege, fcategory, ftype, flang, acysem)
+                if len(deps):
+                    for l in deps.keys():
+                        fdep = l
+                        # print("dep: " + deps[fdep])
+                        if fdep not in dep_list:
+                            dep_list.append(fdep)
+        else:
+            fcollege = ""
+            # print("college: None")
+            deps = get_dep(fcollege, fcategory, ftype, flang, acysem)
+            if len(deps):
+                for l in deps.keys():
+                    fdep = l
+                    # print("dep: " + deps[fdep])
+                    if fdep not in dep_list:
+                        dep_list.append(fdep)
+
+for dep in dep_list:
+    # print(dep)
+    url = "https://timetable.nycu.edu.tw/?r=main/get_cos_list"
+    # User-Agent 請求標頭（request header）含有能令網路協議同級層（peer）識別發出該用戶代理請求的軟體類型或版本號、該軟體使用的作業系統、還有軟體開發者的字詞串。
+    data = {"m_acy": year,          # 學年度(開始)
+            "m_sem": semester,      # 學期別(開始)
+            "m_acyend": year,       # 學年度(結束)
+            "m_semend": semester,   # 學期別(結束)
+            "m_dep_uid": dep,
+            "m_group": "**",
+            "m_grade": "**",
+            "m_class": "**",
+            "m_option": "**",
+            "m_crsname": "**",
+            "m_teaname": "**",
+            "m_cos_id": "**",
+            "m_cos_code": "**",
+            "m_crstime": "**",
+            "m_crsoutline": "**",
+            "m_costype": "**"}
+
+    # 資料型態為 json
+    r = requests.post(url, headers=headers, data=data)
+
+    # requests.codes.ok = 200
+    if r.status_code != requests.codes.ok:
+        print("Data request error!")
+        exit()
+
+    # 將 json 轉換為 python 可處理之資料型態
+    raw_data = json.loads(r.text)
+    for dep_value in raw_data:
+        language = raw_data[dep_value]["language"]
+        for dep_content in raw_data[dep_value]:
+            # if dep_content == "costype":
+            #   pass
+
+            # 課程資料在 dep_content 為 1 或 2 之物件裡
+            if re.match("^[1-2]+$", dep_content) is None:
                 continue
-            raw_cos_data = raw_data[dep_value][dep_content][cos_id]
-            time_list = parse_time(raw_cos_data["cos_time"])
-            classroom_list = parse_classroom(raw_cos_data["cos_time"])
-            brief_code = list(raw_data[dep_value]["brief"][cos_id].keys())[0]
-            brief = raw_data[dep_value]["brief"][cos_id][brief_code]['brief'].split(',')
-            name = raw_cos_data["cos_cname"].replace("(英文授課)", '')
-            name = name.replace("(英文班)", '')
-            course_data[cos_id] = {
-                "id": raw_cos_data["cos_id"],                           # 當期課號
-                "num_limit": raw_cos_data["num_limit"],                 # 人數上限
-                "reg_num": raw_cos_data["reg_num"],                     # 修課人數
-                "name": name,                                           # 課程名稱
-                "credit": raw_cos_data["cos_credit"],                   # 學分
-                "hours": raw_cos_data["cos_hours"],                     # 時數
-                "teacher": raw_cos_data["teacher"],                     # 開課教師
-                "time": time_list,                                      # 上課時間
-                "classroom": classroom_list,                            # 上課教室
-                "time-classroom": raw_cos_data["cos_time"],             # 上課時間及教室
-                "english": language[cos_id]["授課語言代碼"] == "en-us",   # 英語授課
-                "brief": brief,                                         # 摘要
-                "memo": raw_cos_data["memo"],                           # 說明
-                "type": raw_cos_data["cos_type"],                       # 選別
-            }
+            for cos_id in raw_data[dep_value][dep_content]:
+                # 避免加入重複課程
+                if cos_id in course_data:
+                    continue
+                raw_cos_data = raw_data[dep_value][dep_content][cos_id]
+                time_list = parse_time(raw_cos_data["cos_time"])
+                classroom_list = parse_classroom(raw_cos_data["cos_time"])
+                brief_code = list(raw_data[dep_value]["brief"][cos_id].keys())[0]
+                brief = raw_data[dep_value]["brief"][cos_id][brief_code]['brief'].split(',')
+                name = raw_cos_data["cos_cname"].replace("(英文授課)", '')
+                name = name.replace("(英文班)", '')
+                course_data[cos_id] = {
+                    "id": raw_cos_data["cos_id"],                           # 當期課號
+                    "num_limit": raw_cos_data["num_limit"],                 # 人數上限
+                    "reg_num": raw_cos_data["reg_num"],                     # 修課人數
+                    "name": name,                                           # 課程名稱
+                    "credit": raw_cos_data["cos_credit"],                   # 學分
+                    "hours": raw_cos_data["cos_hours"],                     # 時數
+                    "teacher": raw_cos_data["teacher"],                     # 開課教師
+                    "time": time_list,                                      # 上課時間
+                    "classroom": classroom_list,                            # 上課教室
+                    "time-classroom": raw_cos_data["cos_time"],             # 上課時間及教室
+                    "english": language[cos_id]["授課語言代碼"] == "en-us",   # 英語授課
+                    "brief": brief,                                         # 摘要
+                    # "memo": raw_cos_data["memo"],                           # 說明
+                    "type": raw_cos_data["cos_type"],                       # 選別
+                }
 
 with open(str(year) + '-' + str(semester) + "_data.json", "w") as f:
     f.write(json.dumps(course_data))
